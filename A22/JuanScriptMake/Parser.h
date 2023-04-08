9a450847d98ce2e74268ccf1308847ca76981f5a
Luis Resendiz <rese0008@algonquinlive.com>
2023-04-08 16:33:25 -0400

    Fixed A22(Still needs string revisions)

---
+++ A22/JuanScriptMake/Parser.h
@@ -0,0 +1,115 @@
+/*
+************************************************************
+* COMPILERS COURSE - Algonquin College
+* Code version: Winter, 2023
+* Author: Luis Resendiz
+* Professors: Paulo Sousa
+************************************************************
+
+@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+@@@@                                                               @@@@
+@@          /       ==============================        /          @@
+@         ////           @@       @@ #@                  ////         @
+@         ////*          @@ @%  @ @@    @@  @ #@#       /////         @
+@     ((( ////))))   @@  @% @%  @ @@ #@ @@  @ #@@#   ///////( ///     @
+@     ((((,/ )))))    @@@@   @@@  @@ ##  @@@   #@#   ///// ,/////     @
+@     (((((/)))((    ------------------------------    ((((./////*    @
+@    //*./ /  .///   ---  PROGRAMMING LANGUAGE  ---  ////   / ( (//   @
+@    ///// / /////   ==============================  * ////* / ////   @
+@     ///// ///// ((                               (( ///// /////     @
+@    ((((   / , (((((                             (((((  //   /(((    @
+@    (((((((/ . (((((                          (((((* / (((((((       @
+@      (((((( //((((/((((                    *((((/((((/( (((((/      @
+@       .//,   * /   (((((                   (((((  ///    .//.       @
+@     ,////////./(  (((((* ////         (///(((((( ,/(,////////       @
+@         //////// ,// ((( /////,     ////// ((( //  /////// *        @
+@            (((((((((,// * /////     /////   (//(((((((((            @
+@            ((((((((((((/////         //.///  (((((((((((.           @
+@                   (///////// //(   (//.//////////                   @
+@                  (/////////             //////////                  @
+@                                                                     @
+@@          A L G O N Q U I N   C O L L E G E  -  2 0 2 3 W          @@
+@@@@                                                               @@@@
+@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+
+*/
+
+/*
+************************************************************
+* File name: parser.h
+* Compiler: MS Visual Studio 2022
+* Course: CST 8152 – Compilers, Lab Section: [011, 012, 013]
+* Assignment: A32.
+* Date: Sep 01 2022
+* Professor: Paulo Sousa
+* Purpose: This file is the main header for Parser (.h)
+*************************************************************/
+
+#ifndef PARSER_H_
+#define PARSER_H_
+
+/* Inclusion section */
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdarg.h>
+
+#ifndef COMPILERS_H_
+#include "Compilers.h"
+#endif
+#ifndef READER_H_
+#include "Reader.h"
+#endif
+#ifndef SCANNER_H_
+#include "Scanner.h"
+#endif
+
+/* Global vars */
+static Token lookahead;
+extern BufferReader* stringLiteralTable;
+extern juan_int line;
+extern Token tokenizer();
+extern juan_char* keywordTable[];
+juan_int syntaxErrorNumber = 0;
+
+#define STR_LANGNAME	"JUANSCRIPT"
+#define LANG_WRTE		"JUANFULLHD"
+#define LANG_READ		"JUANBALCONY"
+#define LANG_MAIN		"<?JUAN"
+
+/* TO_DO: Create ALL constants for keywords (sequence given in table.h) */
+
+/* Constants */
+enum KEYWORDS {
+	NO_ATTR = -1,
+	KW_data,
+	KW_code,
+	KW_int,
+	KW_real,
+	KW_string,
+	KW_if,
+	KW_then,
+	KW_else,
+	KW_while,
+	KW_do
+};
+
+/* Function definitions */
+juan_void startParser();
+juan_void matchToken(juan_int, juan_int);
+juan_void syncErrorHandler(juan_int);
+juan_void printError();
+
+/* TO_DO: Place ALL non-terminal function declarations */
+juan_void codeSession();
+juan_void dataSession();
+juan_void optVarListDeclarations();
+juan_void optionalStatements();
+juan_void outputStatement();
+juan_void outputVariableList();
+juan_void program();
+juan_void statement();
+juan_void statements();
+juan_void statementsPrime();
+
+#endif
